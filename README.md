1a3363e89af07ee4bb196dae0ddb6fd2a6588981
nirasan <niranirao@gmail.com>
2019-12-19 18:47:16 +0900

    deploy functions

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # cloud-functions-dynamic-page-renderer
+
  Functions that render dynamic pages with Google Cloud Functions
+
+# Development commands
+
+## Run local server
+
+```
+npm run start
+```
+
+## Request to local server
+
+```
+curl -G 'http://localhost:8080' --data-urlencode 'url=http://example.com/' 
+```
+
+# Operation commands
+
+## Deploy
+
+```
+npm run deploy
+```
--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+/**
+ * HTTP triggered function.
+ *
+ * @param {Object} req Cloud Function request context.
+ *                     More info: https://expressjs.com/en/api.html#req
+ * @param {Object} res Cloud Function response context.
+ *                     More info: https://expressjs.com/en/api.html#res
+ */
 exports.dynamicPageRenderer = (req, res) => {
     const url = req.query.url;
     if (!url) {
@@ -13,24 +21,21 @@ exports.dynamicPageRenderer = (req, res) => {
         })
 };
 
+// Get contents.
 async function run(url) {
     const puppeteer = require('puppeteer');
 
     const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
 
     const page = await browser.newPage();
-    page.on('error', err=> {
+    page.on('error', err => {
         console.log('error happen at the page: ', err);
     });
-    page.on('pageerror', pageerr=> {
+    page.on('pageerror', pageerr => {
         console.log('pageerror occurred: ', pageerr);
     });
 
-    await page.goto(url, { waitUntil: 'networkidle0' });
-    await page.setViewport({
-        width: 1200,
-        height: 800
-    });
+    await page.goto(url, {waitUntil: 'networkidle0'});
     await autoScroll(page);
 
     const content = await page.content();
@@ -38,17 +43,23 @@ async function run(url) {
     return content;
 }
 
-async function autoScroll(page){
+// Load lazy loading contents.
+// https://stackoverflow.com/a/53527984
+async function autoScroll(page) {
+    await page.setViewport({
+        width: 1200,
+        height: 800
+    });
     await page.evaluate(async () => {
         await new Promise((resolve, reject) => {
-            var totalHeight = 0;
-            var distance = 100;
-            var timer = setInterval(() => {
-                var scrollHeight = document.body.scrollHeight;
+            let totalHeight = 0;
+            const distance = 100;
+            const timer = setInterval(() => {
+                const scrollHeight = document.body.scrollHeight;
                 window.scrollBy(0, distance);
                 totalHeight += distance;
 
-                if(totalHeight >= scrollHeight){
+                if (totalHeight >= scrollHeight) {
                     clearInterval(timer);
                     resolve();
                 }
