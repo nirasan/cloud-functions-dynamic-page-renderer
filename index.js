f89b8f83f75b5bf5bb320b33a15311a9b5257494
nirasan <niranirao@gmail.com>
2019-12-19 15:51:46 +0900

    impl dynamic page renderer

--- index.js
+++ index.js
@@ -1,3 +1,58 @@
-exports.helloWorld = (req, res) => {
-    res.send('Hello, World');
+exports.dynamicPageRenderer = (req, res) => {
+    const url = req.query.url;
+    if (!url) {
+        return;
+    }
+    run(url)
+        .then((content) => {
+            res.status(200).send(content);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("error: " + err);
+        })
 };
+
+async function run(url) {
+    const puppeteer = require('puppeteer');
+
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+
+    const page = await browser.newPage();
+    page.on('error', err=> {
+        console.log('error happen at the page: ', err);
+    });
+    page.on('pageerror', pageerr=> {
+        console.log('pageerror occurred: ', pageerr);
+    });
+
+    await page.goto(url, { waitUntil: 'networkidle0' });
+    await page.setViewport({
+        width: 1200,
+        height: 800
+    });
+    await autoScroll(page);
+
+    const content = await page.content();
+    browser.close();
+    return content;
+}
+
+async function autoScroll(page){
+    await page.evaluate(async () => {
+        await new Promise((resolve, reject) => {
+            var totalHeight = 0;
+            var distance = 100;
+            var timer = setInterval(() => {
+                var scrollHeight = document.body.scrollHeight;
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+
+                if(totalHeight >= scrollHeight){
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 100);
+        });
+    });
+}
